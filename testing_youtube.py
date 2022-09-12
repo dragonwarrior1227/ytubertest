613184ef62eb944be27187720157b52935d4e3ce
dragonwarrior1227 <vinay2210978@gmail.com>
2022-09-12 10:33:31 +0200

    second

--- testing_youtube.py
+++ testing_youtube.py
@@ -53,7 +53,7 @@ options.add_argument('--disable-dev-shm-usage')
 # options.add_argument("--proxy-server={}".format(py))
 # options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
 # options.add_argument("--incognito")
-options.add_extension('vpn.crx')
+options.add_extension('chrome/vpn.crx')
 
 
 # options.add_experimental_option("debuggerAddress", "127.0.0.1:8989")
