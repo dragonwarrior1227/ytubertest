506fb0aa8c573099fb3422adbcdf2fbaccfa7824
vinay krishna <vinay221097@gmail.com>
2022-09-12 09:47:37 +0200

    first

---
+++ testing_youtube.py
@@ -0,0 +1,103 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import random 
+import socket 
+from webdriver_manager.chrome import ChromeDriverManager
+import os,sys, stat
+import subprocess
+from utilities import *
+
+hostname = socket.gethostname()    
+IPAddr = socket.gethostbyname(hostname)    
+print("Your Computer Name is:" + hostname)    
+print("Your Computer IP Address is:" + IPAddr)
+# options = Options()
+
+
+
+# print(subprocess.Popen("npm install chromium-version@77",shell=True,stdout=subprocess.PIPE).communicate()[0])
+# chrome_path=r"{}/node_modules/chromium-version/lib/chromium/chrome-linux/chrome".format(os.getcwd())
+
+# os.environ['CHROME_PATH']=chrome_path
+# binary_path=os.environ.get('CHROME_PATH')
+
+path=r"chrome/chromedriver"
+# path=r'chromedriver.exe'
+os.chmod(path, 0o777)
+options = Options()
+
+# options.binary_location =binary_path
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+# options.add_argument("--proxy-server={}".format(py))
+# options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
+# options.add_argument("--incognito")
+# options.add_extension('vpn.crx')
+
+
+# options.add_experimental_option("debuggerAddress", "127.0.0.1:8989")
+try:
+    driver = webdriver.Chrome(executable_path=path,chrome_options=options)
+except Exception as e:
+    print(e)
+    driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=options)
+
+
+
+
+
+
+print("Your Computer Name2 is:" + hostname)    
+print("Your Computer IP Address2 is:" + IPAddr)
+print(os.listdir(os.getcwd()))
+
+url = 'https://www.youtube.com/watch?v=ku3HSNT0I-g'
+# url="https://www.youtube.com/watch?v=X2oy3wlbcBs"
+path = 'scrape.png'
+
+channel_url="https://www.youtube.com/channel/UCwXFDb_1KaJMn1xZ6yrdugQ"
+
+# play_via_proxy_browser(driver,url)
+time.sleep(2)
+driver.get(url)
+time.sleep(2)
+set_driver_cookies(driver)
+time.sleep(2)
+play_via_channel_page(driver,channel_url,url)
+# driver.get(url)
+time.sleep(2)
+
+ads_exist=False
+multiple_ads=False
+try:
+    if driver.execute_script("return document.getElementsByClassName('video-ads ytp-ad-module')[0].childElementCount")>0:
+        ads_exist=True
+    if ads_exist==True:
+        if "Ad 1 of 2" in driver.execute_script("return document.getElementsByClassName('ytp-ad-simple-ad-badge')[0].textContent"):
+            multiple_ads=True
+        play_and_sleep(driver)
+    if multiple_ads==True and ("Ad 2 of 2" in driver.execute_script("return document.getElementsByClassName('ytp-ad-simple-ad-badge')[0].textContent")):
+        play_and_skip_ads(driver)
+
+    
+except Exception as e:
+    print(e)
+    pass
+
+play_and_sleep(driver)
+driver.save_screenshot(path)
+driver.quit()
+
+
+
+
+
+
---
+++ utilities.py
@@ -0,0 +1,151 @@
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time,random
+
+
+def set_driver_cookies(driver):
+	print("setting cookies")
+	cookies=[
+
+    {'domain': '.youtube.com', 'expiry': 1696800781, 'httpOnly': True, 'name': 'LOGIN_INFO', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': 'AFmmF2swRAIgeTSsxUyWhTidfq_qLwbF9J9PjopO7kk3EgLSwG2jdy4CIByzmM0oOgZ13nf_Eun1OkPxaLn3lnMeH1XCaleRY1Td:QUQ3MjNmelQzMDlFSDhjb3REVnp1bGtIN2l2WU1oX2NRRDIxRmNTZmttcnM4TTZueTR3cWlZcG1wT3JJMlNscjZvX3FzT2hla0F2Y3RjWkN4RkFHcm8xTm1iNzMySE82UG5ZekVUNkxiUkZyQXAxdWdxTnVxYnJRc3FySFZ5NlhSNGZlS2VfZVh6MmtuWXVyWWEzRFl0eE9PVmotNmh3N29n'}, 
+    {'domain': '.youtube.com', 'expiry': 1696800780, 'httpOnly': False, 'name': 'SID', 'path': '/', 'secure': False, 'value': 'OAjcTQfQ3DWWajAPttwUu1MOMR7-HNujx33PIBs9iZ9qdOftXUuYBT0cDAAOqzLBXpEa5A.'}, 
+    {'domain': '.youtube.com', 'expiry': 1696800756, 'httpOnly': False, 'name': 'CONSENT', 'path': '/', 'secure': True, 'value': 'PENDING+759'}, 
+    {'domain': '.youtube.com', 'expiry': 1696800780, 'httpOnly': True, 'name': 'HSID', 'path': '/', 'secure': False, 'value': 'ALKicwGR6CrqSgocV'},
+    {'domain': '.youtube.com', 'expiry': 1696800780, 'httpOnly': True, 'name': 'SSID', 'path': '/', 'secure': True, 'value': 'A9IY-7Y2ccQWGbw_W'},
+    {'domain': '.youtube.com', 'expiry': 1696800780, 'httpOnly': True, 'name': 'SOCS', 'path': '/', 'secure': True, 'value': 'CAISEwgDEgk0NzEzNjQ0MzgaAmVuIAEaBgiAm9qYBg'}
+    ]
+	for cookie in cookies:
+		driver.add_cookie({'name':cookie['name'],'value':cookie['value']})
+
+	return 
+
+
+
+def play_and_sleep(driver):
+	try:
+		if driver.execute_script(" return document.getElementsByTagName('video')[0].paused")==True:
+			actions = ActionChains(driver)
+			print("paused")
+			actions.send_keys(Keys.SPACE).perform()
+		duration= driver.execute_script(" return document.getElementsByTagName('video')[0].duration")
+		duration=int(duration)
+		print("video duration" ,duration)
+		random_duration=random.randint(duration,duration+5)
+		time.sleep(random_duration)
+	except Exception as e:
+		print(e)
+	return 
+
+def play_and_skip_ads(driver):
+	try:
+		if driver.execute_script(" return document.getElementsByTagName('video')[0].paused")==True:
+			actions = ActionChains(driver)
+			print("paused")
+			actions.send_keys(Keys.SPACE).perform()
+		duration= driver.execute_script(" return document.getElementsByTagName('video')[0].duration")
+		duration=int(duration)
+		print("Ad duration" ,duration)
+		random_duration=random.randint(duration//2,duration)
+		time.sleep(random_duration)
+		if driver.execute_script("return document.getElementsByClassName('ytp-ad-skip-button ytp-button')[0].childElementCount")>0:
+			driver.execute_script("return document.getElementsByClassName('ytp-ad-skip-button ytp-button')[0].click()")
+			print("skipped add")
+	except Exception as e:
+		print(e)
+	return 
+
+
+
+
+def play_via_channel_page(driver,channel_url,video_url):
+	video_id=video_url.split("watch?v=")[1]
+	url=channel_url+"/search?query="+video_id
+	driver.get(url)
+	vide_elements=driver.execute_script("return document.getElementsByClassName('yt-simple-endpoint style-scope ytd-video-renderer')")
+	if len(vide_elements)>0:
+		print("via channel_url")
+		for i in range(0,len(vide_elements)):
+			if video_id in driver.execute_script("return document.getElementsByClassName('yt-simple-endpoint style-scope ytd-video-renderer')["+str(i)+"].href"):
+				print("video by channel search")
+				driver.execute_script("return document.getElementsByClassName('yt-simple-endpoint style-scope ytd-video-renderer')["+str(i)+"].click()")
+				break
+	else:
+		print("direct url")
+		driver.get(video_url)
+
+	return 
+
+def play_via_youtube_search(driver,video_url):
+	video_id=video_url.split("watch?v=")[1]
+	url="https://www.youtube.com/results?search_query="+video_id
+	driver.get(url)
+	vide_elements=driver.execute_script("return document.getElementsByClassName('yt-simple-endpoint style-scope ytd-video-renderer')")
+	if len(vide_elements)>0:
+		print("via youtube search")
+		for i in range(0,len(vide_elements)):
+			if video_id in driver.execute_script("return document.getElementsByClassName('yt-simple-endpoint style-scope ytd-video-renderer')["+str(i)+"].href"):
+				print("video by youtube search")
+				driver.execute_script("return document.getElementsByClassName('yt-simple-endpoint style-scope ytd-video-renderer')["+str(i)+"].click()")
+				break
+	else:
+		print("direct url")
+		driver.get(video_url)
+
+	return 
+
+
+
+
+def direct_play(driver,video_url):
+	driver.get(video_url)
+	return
+
+
+def play_via_embeded(driver,video_url):
+
+	return
+
+def play_via_proxy_browser(driver,video_url):
+	url="https://www.croxyproxy.com/"
+	driver.get(url)
+	time.sleep(2)
+	driver.execute_script("return document.getElementById('url').value='"+video_url+"'")
+	time.sleep(5)
+	driver.find_element(By.ID,"requestSubmit").click()
+
+	return
+
+
+# my_list = [func_test_1, func_test_2, func_test_3]
+# random.choice(my_list)()
+
+
+
+
+
+
+
+
+
+
+
+
+# driver.get("chrome-extension://eppiocemhmnlbhjplcgkofciiegomcon/popup/index.html")
+# time.sleep(3)
+
+# print(driver.window_handles)
+# if len(driver.window_handles)>1:
+#     driver.switch_to.window(window_name=driver.window_handles[1])
+#     driver.close()
+# driver.switch_to.window(window_name=driver.window_handles[0])
+# time.sleep(3)
+
+# btn=driver.find_element(By.XPATH,"/html/body/div/div/div[2]/div/div/div/button[2]").click()
+# time.sleep(1)
+# btn=driver.execute_script("return document.getElementsByClassName('select_location__button-box')[0].children[1].click()")
+# vpn_count=driver.execute_script("return document.getElementsByClassName('locations')[0].childElementCount")
+# random_vpn=random.randint(1,vpn_count)
+# print(vpn_count,random_vpn)
+# driver.execute_script("return document.getElementsByClassName('locations')[0].children[{}].click()".format(random_vpn))
+# time.sleep(2)
