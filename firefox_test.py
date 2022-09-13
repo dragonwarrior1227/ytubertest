ed6005e0ee19d82230086c7641efb437086f8a74
dragonwarrior1227 <vinay2210978@gmail.com>
2022-09-13 22:15:48 +0200

    last

--- firefox_test.py
+++ firefox_test.py
@@ -19,10 +19,13 @@ try:
     # print(subprocess.Popen("tar -jxvf firefox.bz2 -C /usr/local/",shell=True,stdout=subprocess.PIPE).communicate()[0])
     # print(subprocess.Popen("rm -rf /usr/bin/firefox",shell=True,stdout=subprocess.PIPE).communicate()[0])
     # print(subprocess.Popen("ln -s /usr/local/firefox/firefox /usr/bin/firefox",shell=True,stdout=subprocess.PIPE).communicate()[0])
-    # print(subprocess.Popen(" yum localinstall firefox.rpm --skip-broken -Va --nofiles --nodigest",shell=True,stdout=subprocess.PIPE).communicate()[0])
-
-    print(subprocess.Popen("rpm -qa firefox",shell=True,stdout=subprocess.PIPE).communicate()[0])
-    print(subprocess.Popen("yum install firefox",shell=True,stdout=subprocess.PIPE).communicate()[0])
+    # print(subprocess.Popen(" yum localinstall firefox.rpm --skip-broken",shell=True,stdout=subprocess.PIPE).communicate()[0])
+    print(subprocess.Popen("yum install wget",shell=True,stdout=subprocess.PIPE).communicate()[0])
+    print(subprocess.Popen("""wget -O- "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US" | tar -jx -C /usr/local/""",shell=True,stdout=subprocess.PIPE).communicate()[0])
+    print(subprocess.Popen("mv /usr/bin/firefox /usr/bin/backup_firefox",shell=True,stdout=subprocess.PIPE).communicate()[0])
+    print(subprocess.Popen("""echo "exclude=firefox" >> /etc/dnf/dnf.conf""",shell=True,stdout=subprocess.PIPE).communicate()[0])
+    print(subprocess.Popen("""ln -s /usr/local/firefox/firefox /usr/bin/firefox""",shell=True,stdout=subprocess.PIPE).communicate()[0])
+    
     options = Options()
     options.binary_location =r'/usr/bin/firefox'
     options.add_argument('--headless')
